e2c70c1f70af838acab267f617936c206971484c
Ikem Peter <ikempeter2020@gmail.com>
2025-03-29 15:58:06 +0100

    ..

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "~/styles/globals.css";
 
 import type { Metadata } from "next";
-// import { Geist } from "next/font/google";
+import { Geist } from "next/font/google";
 
 import { TRPCReactProvider } from "~/trpc/react";
 
@@ -11,10 +11,10 @@ export const metadata: Metadata = {
 	icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-// const geist = Geist({
-// 	subsets: ["latin"],
-// 	variable: "--font-geist-sans",
-// });
+const geist = Geist({
+	subsets: ["latin"],
+	variable: "--font-geist-sans",
+});
 
 export default function RootLayout({
 	children,
